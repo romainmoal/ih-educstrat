4b1b0848e177b349fd7cd75f848d0d5bc834e010
Romain MOAL <romain.moal.03@gmail.com>
2019-05-14 18:58:13 +0200

    apres vac

---
+++ models/Project.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const ProjectProfile = new Schema({
+  general: {
+    imgUrl: {type: String},
+    fullName: {type: String},
+    headline: {type: String},
+    school: {type: String},
+    location: {type: String},
+    connections: {type: Number},
+    profileUrl: {type: String},
+    connectionDegree: {type: String},
+    vmid: {type: String},
+    linkedinSalesNavigatorUrl: {type: String},
+    mutualConnectionsUrl: {type: String},
+    description: {type: String},
+    firstName: {type: String},
+    lastName: {type: String}
+  },
+  jobs: {
+    // a ajouter, le numéro du job ?
+    companyName: String,
+    companyUrl: String,
+    jobTitle: String,
+    dateRange: String,
+    location: String,
+    description: String
+  },
+  schools: {
+    schoolUrl: String,
+    schoolName: String,
+    degree: String,
+    dateRange: String
+  },
+  details: {
+    linkedinProfile: String
+  },
+  skills: String,
+  allSkills: String
+});
+
+
+const Profile = mongoose.model('Project', ProjectProfile);
+module.exports = Profile;
