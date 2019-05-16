0976f90c564ac71d572ce9d185b50ebba0722ba6
Mathias Zemiro <mathiaszemiro@MacBook-Air-de-Mathias.local>
2019-05-16 21:32:27 +0200

    Mathias Sankey Chart

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,32 @@
-const express = require('express');
-const router  = express.Router();
-const Profile = require('../models/Profile.js');
+const express = require("express");
+const router = express.Router();
+const Profile = require("../models/Profile.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/main', (req, res, next) => {
+router.get("/main", (req, res, next) => {
   Profile.find()
-    .then(testProfile => {
-      console.log('Retrieved books from DB:', testProfile);
-      res.render('main', { testProfile: testProfile });
+    .then(ProfileModel => {
+      console.log("Retrieved books from DB:", ProfileModel);
+      res.render("main", { ProfileModel: ProfileModel });
     })
     .catch(error => {
-      console.log('Error while getting the profile from the DB: ', error);
-    })
+      console.log("Error while getting the profile from the DB: ", error);
+    });
 });
 
+router.get("/sankey", (req, res, next) => {
+  Profile.find()
+    .then(ProfileModel => {
+      console.log("Retrieved results from DB:", ProfileModel);
+      res.render("sankey", { ProfileModel: ProfileModel });
+    })
+    .catch(error => {
+      console.log("Error while getting the profile from the DB: ", error);
+    });
+});
 
 module.exports = router;
