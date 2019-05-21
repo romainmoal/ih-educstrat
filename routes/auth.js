f43718d185074b1ea6c57d3085860f8d27dcc284
Mathias Zemiro <mathiaszemiro@MacBook-Air-de-Mathias.local>
2019-05-21 20:28:31 +0200

    W/ working sankey search

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
 
 // User model
-const User           = require("../models/User");
+const User = require("../models/User");
 
 // BCrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 // signup
-router.get('/signup', (req, res, next) => {
-  res.render('auth/signup');
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
 });
 
 router.post("/signup", (req, res, next) => {
@@ -21,37 +21,46 @@ router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  if (lastname === "" || firstname === "" || username === "" || password === "" ) {
-    res.render("auth/signup", { errorMessage: "All the fields are compulsory" });
+  if (
+    lastname === "" ||
+    firstname === "" ||
+    username === "" ||
+    password === ""
+  ) {
+    res.render("auth/signup", {
+      errorMessage: "All the fields are compulsory"
+    });
     return;
   }
 
   User.findOne({ username })
-  .then(user => {
-    if (user !== null) {
-      res.render("auth/signup", { errorMessage: "The username already exists" });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        res.render("auth/signup", { errorMessage: "Something went wrong" });
-      } else {
-        res.redirect("/");
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
       }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save(err => {
+        if (err) {
+          res.render("auth/signup", { errorMessage: "Something went wrong" });
+        } else {
+          res.redirect("/");
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
     });
-  })
-  .catch(error => {
-    next(error)
-  })
 });
 
 // log in
@@ -59,13 +68,16 @@ router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
-router.post("/login", passport.authenticate("local", {
-  // successRedirect: "/home/:id",
-  successRedirect: "/main",
-  failureRedirect: "/",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    // successRedirect: "/home/:id",
+    successRedirect: "/main",
+    failureRedirect: "/",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 router.post("/login", (req, res, next) => {
   const theUsername = req.body.username;
@@ -78,8 +90,8 @@ router.post("/login", (req, res, next) => {
     return;
   }
 
-  User.findOne({ "username": theUsername })
-  .then(user => {
+  User.findOne({ username: theUsername })
+    .then(user => {
       if (!user) {
         res.render("auth/login", {
           errorMessage: "The username doesn't exist."
@@ -95,10 +107,10 @@ router.post("/login", (req, res, next) => {
           errorMessage: "Incorrect password"
         });
       }
-  })
-  .catch(error => {
-    next(error);
-  })
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 // update profile
@@ -107,24 +119,27 @@ router.get("/profile/edit", (req, res, next) => {
 });
 
 router.get("/profile/edit", (req, res, next) => {
-  User.findOne({_id: req.query._id})
-  .then((user) => {
-    res.render("auth/update", {user});
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+  User.findOne({ _id: req.query._id })
+    .then(user => {
+      res.render("auth/update", { user });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 router.post("/profile/edit", (req, res, next) => {
   const { firstname, lastname, username, password } = req.body;
-  User.update({_id: req.query.book_id}, { $set: { firstname, lastname, username, password }})
-  .then((user) => {
-    res.redirect('/main');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+  User.update(
+    { _id: req.query.book_id },
+    { $set: { firstname, lastname, username, password } }
+  )
+    .then(user => {
+      res.redirect("/main");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 // log out
